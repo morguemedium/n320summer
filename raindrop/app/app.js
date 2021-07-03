5eba6ce5505b961ff6beca74d2945dbfbb415ee0
morguemedium <77863305+morguemedium@users.noreply.github.com>
2021-07-03 17:42:57 -0400

    updates

--- raindrop/app/app.js
+++ raindrop/app/app.js
@@ -5,19 +5,22 @@ class Drop{
         this.y = cy;
         this.rad = rad;
         this.col = col;
+        this.speed = 1 + Math.random() * 3;
     }
     update(){
-        this.y = this.y + 5;
+        this.y = this.y + this.speed;
+        this.speed = this.speed + .5;
         fill(this.col);
         circle(this.x,this.y,this.rad);
     }
 };
 
 var mulCircles = [];
-mulCircles[0] = new Drop(35,0,30,[85, 147, 181]);
-mulCircles[1] = new Drop(135,0,30,[85, 147, 181]);
-mulCircles[2] = new Drop(235,0,30,[85, 147, 181]);
-mulCircles[3] = new Drop(335,0,30,[85,147,181]);
+mulCircles[0] = new Drop(35,0,30,[171, 198, 235]);
+mulCircles[1] = new Drop(135,0,30,[171, 198, 235]);
+mulCircles[2] = new Drop(235,0,30,[171, 198, 235]);
+mulCircles[3] = new Drop(335,0,30,[171, 198, 235]);
+mulCircles[4] = new Drop(435,0, 30,[171, 198, 235]);
 
 class Ground{
     constructor(gx,gy,gColor){
@@ -25,17 +28,22 @@ class Ground{
         this.gy= gy;
         this.gColor= gColor;
     }
-}
+    updateG(){}
+};
+
+let hitGround = new Ground (0,500,30,[0,5,0]);
 
 function setup(){
     createCanvas(500,500);
-    background(177, 242, 242);
+    background(223, 233, 247);
 }
 
 function draw(){
-    background(177, 242, 242);
+    background(223, 233, 247);
 
     mulCircles[0].update();
     mulCircles[1].update();
     mulCircles[2].update();
+    mulCircles[3].update();
+    mulCircles[4].update();
 }

d9ff98f8977260d21709116bf417292f1141055c
morguemedium <77863305+morguemedium@users.noreply.github.com>
2021-07-23 16:44:20 -0400

    update

--- react_lab/js/app.js
+++ react_lab/js/app.js
@@ -1,36 +1,50 @@
+import "/styles.css";
 import React from "react";
 
-export default class RecipeSearch extends React.Component{
-    //components can be defined as classes or functions
-    //MUST define render
-    state = { 
-        recipes: [
-            "Apple Pie",
-            "Cherry Pie",
-            "Pizza Pie",
-            "Hamburger", 
-            "Ham Steak", 
-            "Chocolate Mousse", 
-            "Chocolate Ice Cream"
-        ],
-        searchTerm: ""
-    }
-    //the state variable contains the array that has the list of all the recipes
-    
-    render(){
-        let recipesList = this.state.recipes.map((recipe) =>{
-            return <li>{recipe}</li>;
-        });
-        //this variable is connected to state array, recipes array
-        //map object holds key/value pairs & remembers order
-        
-        return(
-            <div className="recipes">
-                <h2>Recipes</h2>
-                <input value={this.state.searchTerm}
-                onChange={(event)=> { this.updateSearch }}></input>
-                <ul>{recipesList}</ul>
-            </div>
-        )
-    }
-};
+export default class RecipeSearch extends React.Component {
+  //components can be defined as classes or functions
+  //MUST define render
+  state = {
+    recipes: [
+      "Apple Pie",
+      "Cherry Pie",
+      "Pizza Pie",
+      "Hamburger",
+      "Ham Steak",
+      "Chocolate Mousse",
+      "Chocolate Ice Cream"
+    ],
+    searchTerm: ""
+  };
+  //the state variable contains the array that has the list of all the recipes
+
+  render() {
+    //pull out recipes that match search terms
+    let matchedRecipes = this.state.recipes.filter((recipe) => {
+      return recipe.toLocaleUpperCase().includes(this.state.searchTerm);
+    });
+
+    let recipesList = matchedRecipes.map((recipe) => {
+      return <li>{recipe}</li>;
+    });
+    //this variable is connected to state array, recipes array
+    //map object holds key/value pairs & remembers order
+
+    return (
+      <div className="recipes">
+        <h2>Recipes</h2>
+        <input
+          value={this.state.searchTerm}
+          onChange={(event) => {
+            this.updateSearch(event);
+          }}
+        ></input>
+        <ul>{recipesList}</ul>
+      </div>
+    );
+  }
+
+  updateSearch(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+}

498b1aa67230f5cd35e0496b6063833a500d72d0
morguemedium <77863305+morguemedium@users.noreply.github.com>
2021-07-25 20:01:10 -0400

    grid

--- connect/js/app.js
+++ connect/js/app.js
@@ -1,4 +1,29 @@
-class Game{
-    //let's store variables
-    canvas = document.getElementById("canvas");
+class GameBoard{
+    //properties
+
+    playerOne = 0;
+    playerTwo = 0;
+    playerOneTokens = 0;
+    playerTwoTokens = 0;
+    maxTokens = 0;
+    gameBoard = document.getElementById("gameBoard");
+
+    constructor(){
+        //let's make the grid
+        for(var c = 1; c < 7; c++){
+            for(var q = 1; q < 7; q++){
+            let blankCirc = document.createElementNS("http://www.w3.org/2000/svg","circle");
+
+            blankCirc.setAttribute("cx", 110 * c);
+            blankCirc.setAttribute("cy", 90 * q);
+            blankCirc.setAttribute("r", 30);
+            blankCirc.setAttribute("fill", "#f5eded");
+            this.gameBoard.appendChild(blankCirc);
+
+            console.log("help me");
+        }
+    }
 }
+}
+
+let g = new GameBoard();

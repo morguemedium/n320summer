7d521f88e36c3d4d9978573124a578a322feff0a
morguemedium <77863305+morguemedium@users.noreply.github.com>
2021-07-26 22:38:25 -0400

    struggle......2!

--- connect/js/app.js
+++ connect/js/app.js
@@ -2,6 +2,7 @@ class GameBoard{
     //connect to index file
 
     gameBoard = document.getElementById("gameBoard");
+    currentPlayer = 1;
 
     constructor(){
         
@@ -9,14 +10,29 @@ class GameBoard{
         for(var c = 1; c < 7; c++){
             for(var q = 1; q < 7; q++){
             let blankCirc = document.createElementNS("http://www.w3.org/2000/svg","circle");
-
+            
+            blankCirc.classList.add("boardCirc");
             blankCirc.setAttribute("cx", 110 * c);
             blankCirc.setAttribute("cy", 90 * q);
             blankCirc.setAttribute("r", 30);
             blankCirc.setAttribute("fill", "#f5eded");
             blankCirc.addEventListener("click",changeToken);
+
+            for (let s = 0; s < blankCirc.length; s++){
+                blankCirc[s].onclick = () => {
+                    if (blankCirc[s + 7]){
+                        blankCirc[s].classList.add("selected");
+                        blankCirc[s].classList.add("player-one");
+                        currentPlayer = 2;
+                    } else if(this.currentPlayer = 2){
+                        blankCirc[s].classList.add("selected");
+                        blankCirc[s].classList.add("player-two");
+                    }
+                }
+            }
            
             this.gameBoard.appendChild(blankCirc);
+
 
             console.log("help me");
         }
@@ -25,8 +41,7 @@ class GameBoard{
 };
 
 function changeToken(event){
-    let currentPlayer = 1;
-    event.target.setAttribute("fill", "#d988f2");
+    event.target.getAttribute("fill", "#d488f2");
     
 }
 
